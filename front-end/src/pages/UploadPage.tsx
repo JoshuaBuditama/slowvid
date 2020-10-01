bc99986111a8f7e22e07558ce0cc7956a865f267
Joshua Buditama <joshuakent46@yahoo.com>
2020-10-01 17:53:04 +0930

    added the cancel and submit buttons

--- front-end/src/pages/UploadPage.tsx
+++ front-end/src/pages/UploadPage.tsx
@@ -13,10 +13,39 @@ let Page = (props: RouteComponentProps) => (
       
       {/*Input column*/}
       <div className="columns is-centered">
-          <div className="column is-half">
-            <input className="input" type="text" placeholder="Please type the code here"/>
+        <div className="column is-half">
+          <input className="input" type="text" placeholder="Please type the code here"/>
+        </div>
+      </div>
+
+      {/*Text below the text input*/}
+      <h6 className="subtitle is-6">
+        Only authorised health care professionals can provide a valid health care code
+      </h6>
+
+      <div className="columns is-centered">
+        {/*Cancel button*/}
+        <div className="column is-half">
+          <div className="field">
+            <p className="control">
+              <button className="button">
+                Cancel
+              </button>
+            </p>
           </div>
         </div>
+
+        {/*Submit button*/}
+        <div className="column is-half">
+          <div className="field">
+            <p className="control">
+              <button className="button is-link">
+                I consent to sharing my close contacts
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </body>
