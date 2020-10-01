a7bfb35ba4b6858e5348037880d079dcd6eef275
Joshua Buditama <joshuakent46@yahoo.com>
2020-10-01 16:44:50 +0930

    Added the texts above the input box

--- front-end/src/pages/UploadPage.tsx
+++ front-end/src/pages/UploadPage.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
-let Page = (props: RouteComponentProps) => <div>Upload page goes here</div>;
+let Page = (props: RouteComponentProps) => (
+<body>
+  {/**Container for the texts above the input box */}
+  <section className="section is-medium has-text-centered">
+    <div className="container">
+      <h1 className="title">Sharing my close contacts with healthcare professional</h1>
+      <h2 className="subtitle">
+        Please enter the healthcare code provided to you by the healthcare professional
+      </h2>
+    </div>
+  </section>
+</body>
+);
 
 export default Page;
