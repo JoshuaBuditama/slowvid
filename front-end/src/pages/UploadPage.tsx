5a8e6c9e985d771e52d4a30f025bbb3897795b27
Joshua Buditama <joshuakent46@yahoo.com>
2020-10-01 17:19:06 +0930

    added text input field below the header

--- front-end/src/pages/UploadPage.tsx
+++ front-end/src/pages/UploadPage.tsx
@@ -3,13 +3,20 @@ import { RouteComponentProps } from "@reach/router";
 
 let Page = (props: RouteComponentProps) => (
 <body>
-  {/**Container for the texts above the input box */}
+  {/**Header section*/}
   <section className="section is-medium has-text-centered">
     <div className="container">
       <h1 className="title">Sharing my close contacts with healthcare professional</h1>
       <h2 className="subtitle">
         Please enter the healthcare code provided to you by the healthcare professional
       </h2>
+      
+      {/*Input column*/}
+      <div className="columns is-centered">
+          <div className="column is-half">
+            <input className="input" type="text" placeholder="Please type the code here"/>
+          </div>
+        </div>
     </div>
   </section>
 </body>
