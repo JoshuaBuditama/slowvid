444744928686563d1c55639936d4fd346a237412
Harkaranveer <a1724269@adelaide.edu.au>
2020-10-08 23:43:19 +1030

    health advice message added

--- front-end/src/components/Notifications.tsx
+++ front-end/src/components/Notifications.tsx
@@ -10,6 +10,20 @@ function Positive()
                 <p className="is-size-3 has-text-centered">You have been in close contact with a person
                     who has COVID-19. Please <span style={{color: "red"}}><b>self isolate</b></span> and report to a <span style={{color: "red"}}><b>health
                     clinic</b></span> or a <span style={{color: "red"}}><b>health care professinal</b></span> </p>
+                <br></br>
+                <h1 className="is-size-3">Quarantine at home and watch for symptoms for 14 days:</h1>
+                <ol>
+                    <li>Watch for symptoms, even if mild, of cough, shortness of breath or difficulty breathing, fever,
+                    chills, repeated shaking with chills, fatigue, muscle pain or body aches, headache, sore
+                    throat, new loss of taste or smell, congestion or runny nose, nausea or vomiting, or diarrhea.</li>
+                    <li>Don’t leave home, except to get medical care. Wear a cloth mask if you need to leave home.</li>
+                    <li>Call ahead before visiting a health care provider or emergency department. </li>
+                    <li>If possible, stay in a specific room in your home and use a separate bathroom. </li>
+                    <li>Stay at least six feet or two meters away from others in your home at all times. Wear a cloth
+                    mask if you’re in any room with other people, unless you have trouble breathing. Don’t share
+                    household items. </li>
+                    <li>Stay connected with others – use technology to communicate with friends and family. </li>
+                </ol>  
             </div>
         </div>
     );
