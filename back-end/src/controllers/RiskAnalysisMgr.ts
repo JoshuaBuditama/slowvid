fb9614b2d6adf0a7c8941233be0f4b031cceb2b2
Craig S. Keogh <craig.s.keogh@gmail.com>
2020-10-11 16:22:11 +1030

    Handle empty lists, issue #66

--- back-end/src/controllers/RiskAnalysisMgr.ts
+++ back-end/src/controllers/RiskAnalysisMgr.ts
@@ -23,13 +23,17 @@ function thresholdMet(upload: IUploadTableDocument): boolean {
  * Search all the upload table tokens against a list of query encounter tokens.
  * Use when a user wants to check if they have come in contact with confirmed
  * positive. Set's user's `closeContactFlag` if they have come in contact with
- * confirmed positive. Assumes that confirmed positives have upload tables,
- * empty otherwise.
+ * confirmed positive AND meets the threshold set in `thresholdMet`. Assumes
+ * that confirmed positives have upload tables, empty otherwise.
  * @param  {IQueryTableDocument[]} queryTable list of query encounter tokens
  * @returns {Promise<boolean>} true if match was found (i.e. one or more of the
- *     query encounter tokens have come in contact with confirmed case).
+ *     query encounter tokens have come in contact with confirmed case AND meets
+ *     threshold set in `thresholdMet`).
  */
 export async function searchUploadEncounters(queryTable: IQueryTableDocument[]): Promise<boolean> {
+	if (queryTable === null || queryTable === undefined || queryTable.length < 1) {
+		return false;
+	}
 	let res = await UploadTableModel.find(
 		{
 			$or: queryTable.map(
@@ -52,18 +56,24 @@ export async function searchUploadEncounters(queryTable: IQueryTableDocument[]):
  * Search all the query table tokens against a list of upload encounter tokens.
  * Use when adding a new confirmed positive and wish to flag all the query users
  * who have come in contact with the confirmed positive. Set's all user's
- * `closeContactFlag` if they have come in contact with confirmed positive.
+ * `closeContactFlag` if they have come in contact with confirmed positive AND
+ * meets the threshold set in `thresholdMet`.
  * @param  {IUploadTableDocument[]} uploadTable list of upload encounter tokens 
  * @returns {Promise<IQueryTableDocument[]>} list of matching query encounter
- *     tokens, with populated user documents.
+ *     tokens AND meets the threshold set in `thresholdMet`, with populated
+ *     associated user documents.
  */
 export async function searchQueryEncounters(uploadTable: IUploadTableDocument[]): Promise<IQueryTableDocument[]> {
-	uploadTable.filter(x => thresholdMet(x));
-
+	if (uploadTable === null || uploadTable === undefined || uploadTable.length < 1) {
+		return [];
+	}
+	const filteredTable = uploadTable.filter(x => thresholdMet(x));
+	if (filteredTable.length < 1) {
+		return [];
+	}
 	let res = await QueryTableModel.find(
 		{
-			$or: uploadTable.filter(x => thresholdMet(x))
-				.map(y => ({ encounterToken: y.encounterToken }))
+			$or: filteredTable.map(y => ({ encounterToken: y.encounterToken }))
 		}).populate(UserModel.modelName).exec();
 	for (let i of res) {
 		if (!i.user.closeContactFlag) {
