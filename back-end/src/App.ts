0ca5fedf66837d8a192e8aeb321a6514776c35e5
a1708839 <a1708839@student.adelaide.edu.au>
2020-10-12 13:36:23 +1030

    Front-end and backend implement  OTP verification
    using twilio API to implement the phone number
     verification.

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -1,22 +1,30 @@
 import express from 'express';
-import { Database } from './Model/Database'
-import { UserModel} from './Model/User'
+// import { Database } from './Model/Database'
+// import { UserModel} from './Model/User'
 
-// Create a new express application instance
-const app: express.Application = express();
+// // Create a new express application instance
+// const app: express.Application = express();
 
-let db = new Database()
-db.connect("mongodb://localhost:27017/slowvid");
-UserModel.create({userId: "HASH_ID_TEST"});
+// let db = new Database()
+// db.connect("mongodb://localhost:27017/slowvid");
+// UserModel.create({userId: "HASH_ID_TEST"});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
-process.on('exit', function () {
-  db.disconnect();
-});
+// process.on('exit', function () {
+//   db.disconnect();
+// });
+
+require('dotenv').config();
+
+const app = require('../src/app.js');
+
+app.listen(8000, () => {
+    console.log('Listening on port 8000');
+})
--- front-end/src/pages/SignInPage.tsx
+++ front-end/src/pages/SignInPage.tsx
@@ -1,6 +1,16 @@
+// import React from "react";
+// import { RouteComponentProps } from "@reach/router";
+
+// let Page = (props: RouteComponentProps) => <div>Sign in page goes here</div>;
+
+// export default Page;
+
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
+import SignIn from "../components/Signin.js";
 
-let Page = (props: RouteComponentProps) => <div>Sign in page goes here</div>;
-
-export default Page;
+function notification (props: RouteComponentProps)
+{
+  return <SignIn />
+}
+export default notification;
