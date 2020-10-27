a1393cab0cbe271661045994794c16db084bc336
Joshua Buditama <joshuakent46@yahoo.com>
2020-10-27 16:45:31 +1030

    Changed the ability to close the pop-up from anywhere to just one button

--- front-end/src/components/PopUpElement.tsx
+++ front-end/src/components/PopUpElement.tsx
@@ -10,13 +10,13 @@ function PopUpElement({type, onPopUpClick}){
   return(
     <div className={`${type?"is-clipped":""}`}>
       {type && <div className={`modal ${type?"is-active":""}`}>
-        <div className="modal-background" onClick={closePopUp}></div>
-          <div className="modal-content" onClick={closePopUp}>
+        <div className="modal-background"></div>
+          <div className="modal-content">
             {type === "upload-success" && <section className="section has-text-centered">
-              <UploadSuccess></UploadSuccess>
+              <UploadSuccess onPopUpClick={closePopUp}></UploadSuccess>
             </section>}
           </div>
-        <button className="modal-close is-large" aria-label="close" onClick={closePopUp}></button>
+        <button className="modal-close is-large" aria-label="close"></button>
       </div>}
     </div>
   );
--- front-end/src/components/UploadSuccess.tsx
+++ front-end/src/components/UploadSuccess.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-function UploadSuccess(){
+function UploadSuccess({onPopUpClick}){
+  function closeNotification()
+  {
+    onPopUpClick();
+  }
   return(
     <div className="box">
       <h1 className="title"> 
@@ -10,8 +14,9 @@ function UploadSuccess(){
         Thank you for helping us to make your community safer.
       </h2>
       <h2 className="subtitle">
-        Please click anywhere to close this notification.
+        Please press the 'Close Notification' button to remove this notification.
       </h2>
+      <button className="button is-success" onClick={closeNotification}>Close Notification</button>
     </div>
   );
 }
