0a16c87b37807e6a741dd17a4f2bdca29f97fc82
Jake Brown <jake@eyespacelenses.com>
2020-10-28 21:41:43 +1030

    Added docstrings

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -6,14 +6,25 @@ import { UserModel} from './Model/User'
 const app: express.Application = express();
 
 let db = new Database()
-db.connect("mongodb://localhost:27017/slowvid");
+let dbUrl = process.env.DB_URL || "mongodb://localhost:27017/slowvid"
+let port = process.env.PORT || 3000
+console.log("Starting on port: " +port)
+console.log("Connected to db: " +dbUrl)
+db.connect(dbUrl);
 UserModel.create({userId: "HASH_ID_TEST"});
 
+// Required by App Engine for liveness check
 app.get('/', function (req, res) {
+  res.send('Liveness check OK');
+});
+
+
+// API routes prefixed so we can serve both from the same host (no CORS required)
+app.get('/api/hello', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
 
