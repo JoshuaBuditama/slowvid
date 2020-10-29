b439ec3f86b501571b4a4ad034210c0e58aba81e
Craig S. Keogh <craig.s.keogh@gmail.com>
2020-10-29 22:43:11 +1030

    Use async for cleaner code #82

--- back-end/src/controllers/MainController.ts
+++ back-end/src/controllers/MainController.ts
@@ -5,63 +5,49 @@ import { HCPUserModel } from '../model/HCPUser';
 import { UserModel } from '../model/User';
 
 export const register = async (req: express.Request, res: express.Response) => {
-	HCPUserModel.findOne({ emailAddress: req.body.emailAddress })
-		.then(user => {
-			if (user) {
-				let error = 'Email Address Exists in Database.';
-				return res.status(400).json(error);
-			} else {
-				const newUser = new HCPUserModel({
-					emailAddress: req.body.emailAddress,
-					password: req.body.password
-				});
-				bcrypt.genSalt(10, (err, salt) => {
-					if (err) throw err;
-					bcrypt.hash(newUser.password, salt,
-						(err, hash) => {
-							if (err) throw err;
-							newUser.password = hash;
-							newUser.save().then(user => res.json(user))
-								.catch(err => res.status(400).json(err));
-						});
-				});
-			}
-		});
-
-};
+	try {
+		let user = await HCPUserModel.findOne({ emailAddress: req.body.emailAddress });
+		if (user) {
+			return res.status(400).json("Email already exists");
+		} else {
+			const newUser = new HCPUserModel({
+				emailAddress: req.body.emailAddress,
+				password: req.body.password
+			});
+			const salt = await bcrypt.genSalt(10);
+			const hash = await bcrypt.hash(newUser.password, salt);
+			newUser.password = hash;
+			await newUser.save();
+			res.status(200).json(user);
+		}
+	} catch (err: any) {
+		res.status(400).json("Error occured during register")
+	}
+}
 
 export const login = async (req: express.Request, res: express.Response) => {
 	const email = req.body.emailAddress;
 	const password = req.body.password;
-	HCPUserModel.findOne({ emailAddress: email })
-		.then(user => {
-			if (!user) {
-				return res.status(404).json("No Account Found");
-			}
-			bcrypt.compare(password, user.password)
-				.then(isMatch => {
-					if (isMatch) {
-						const payload = {
-							id: user._id,
-							emailAddress: user.emailAddress
-						};
-						jwt.sign(payload, "secret", { expiresIn: 36000 }, //TODO
-							(err, token) => {
-								if (err) res.status(500)
-									.json({
-										error: "Error signing token",
-										raw: err
-									});
-								res.json({
-									success: true,
-									token: `Bearer ${token}`
-								});
-							});
-					} else {
-						res.status(400).json("Password is incorrect");
-					}
-				});
-		});
+	try {
+		const user = await HCPUserModel.findOne({ emailAddress: email });
+		if (!user) {
+			return res.status(404).json("Account not found");
+		}
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (isMatch) {
+			const payload = {
+				id: user._id,
+				emailAddress: user.emailAddress
+			};
+			const token = jwt.sign(payload, "secret", { expiresIn: 36000 }); //TODO
+			res.json({
+				success: true,
+				token: `Bearer ${token}`
+			});
+		}
+	} catch (err: any) {
+		res.status(400).json("Error occured during register")
+	}
 }
 
 export const confirm = async (req: express.Request, res: express.Response) => {
--- back-end/src/controllers/PassportConfig.ts
+++ back-end/src/controllers/PassportConfig.ts
@@ -8,22 +8,24 @@ const opts = {
 	secretOrKey: secret
 };
 
-//this sets how we handle tokens coming from the requests that come
-// and also defines the key to be used when verifying the token.
 export function setupPassport(passport: passport.PassportStatic)
 {
 	passport.use(
-		new Strategy(opts, (payload, done) => {
-			HCPUserModel.findById(payload.id)
-				.then(user => {
-					if (user) {
-						return done(null, {
-							id: user.id,
-							email: user.emailAddress,
-						});
-					}
+		new Strategy(opts, async (payload, done) => {
+			try {
+				const user = await HCPUserModel.findById(payload.id);
+				if (user) {
+					return done(null, {
+						id: user.id,
+						email: user.emailAddress,
+					});
+				}
+				else {
 					return done(null, false);
-				}).catch(err => console.error(err));
+				}
+			} catch (err: any) {
+				console.error(err);
+			}
 		})
 	);
 }
