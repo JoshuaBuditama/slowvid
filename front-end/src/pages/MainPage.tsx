8a1d021376a0e4130cc3b3ec3a09a49cba77adf9
ElijahHughes <elijah9999@gmail.com>
2020-10-29 19:07:39 +1030

    added basic main page

--- front-end/src/pages/MainPage.tsx
+++ front-end/src/pages/MainPage.tsx
@@ -2,7 +2,47 @@ import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
 function Home(props: RouteComponentProps) {
-  return <p>Main page. TODO: Implement in <a href="https://github.com/JoshuaBuditama/slowvid/issues/46">Issue 46</a></p>;
+  return (
+      <div>
+	      <section className="section is-medium has-text-centered">
+		      <div className="container">
+		        <p className="is-size-3 has-text-centered">Slowvid is active
+		        </p>
+		        <br/>
+		        <br/>
+		          <div className="column is-half">
+		            <div className="field">
+		              <p className="control">
+			              <form action="notifications">
+			                <input
+			                  type="submit"
+			                  className="button is-link"
+			                  value="Share my close contacts with a health care professional"
+			                />
+		                </form>
+		              </p>
+		            </div>
+		          </div>
+		       	<br/>
+		          <div className="column is-half">
+		            <div className="field">
+		              <p className="control">
+		              	<form action="">
+			                <input
+			                  type="submit"
+			                  className="button is-link"
+			                  value="Your security and privacy is SlowVID's first priority. Learn how"
+			                />
+		                </form>
+		              </p>
+		            </div>
+		          </div>
+		      </div>
+	      </section>
+    </div>
+
+
+  );
 }
 
 export default Home;
