d904630b3ce6d641f2b8ddc3d5b097946dbc66c0
Harkaranveer Singh <lally.singh@live.com>
2020-10-31 03:15:58 +1030

    session wrapper updated

--- front-end/src/Conf.ts
+++ front-end/src/Conf.ts
@@ -8,6 +8,8 @@ export const bluetoothReconnection: boolean = false;
 // Clear browser local storage (Slowvid keys only) at startup
 export const clearLocalStorageOnStartup: boolean = true;
 
+export const backendAddr: string = "http://localhost:3002/api";
+
 export const checkCloseContactFlagPeriodMilliseconds: number = 10000;
 //remove when issues #23 and #25 are implemented
 export const deviceId: string = "deviceId";
