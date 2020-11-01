d68f73a1a53655712d3b84035f59a0db86cf3b25
Harkaranveer Singh <51188481+HarkaranveerSingh@users.noreply.github.com>
2020-11-01 19:37:31 +1030

    Update front-end/src/Conf.ts
    Co-authored-by: Craig S. Keogh <craig.s.keogh@gmail.com>

--- front-end/src/Conf.ts
+++ front-end/src/Conf.ts
@@ -8,7 +8,7 @@ export const bluetoothReconnection: boolean = false;
 // Clear browser local storage (Slowvid keys only) at startup
 export const clearLocalStorageOnStartup: boolean = true;
 
-export const backendAddr: string = "http://localhost:3002/api";
+export const backendAddr: string = "http://localhost:3000/http";
 
 export const checkCloseContactFlagPeriodMilliseconds: number = 10000;
 //remove when issues #23 and #25 are implemented
